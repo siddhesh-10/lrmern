181dea48226f64702ed309ba33b4df0cfbd9f24b
siddhesh1010 <siddheshalagade@gmail.com>
2022-01-10 13:45:23 +0530

    only with email

---
+++ app.js
@@ -0,0 +1,90 @@
+const express =require("express");
+const bodyParse=require("body-parser");
+const ejs=require("ejs");
+const mongoose=require("mongoose");
+var encrypt = require('mongoose-encryption');
+require('dotenv').config();
+
+const app=express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParse.urlencoded({
+    extended : true
+}));
+
+mongoose.connect('mongodb://localhost:27017/lrtry',
+  {
+    useNewUrlParser: true
+  }
+);
+
+const userSchema =new mongoose.Schema( {
+    email : String,
+    password : String
+});
+const secret = process.env.star;
+
+userSchema.plugin(encrypt, { secret: secret,encryptedFields : ["password"] });
+
+
+const user=new mongoose.model("User",userSchema);
+app.get("/",function(req,res){
+    res.render("home")
+})
+
+app.get("/login",function(req,res){
+    res.render("login")
+})
+
+app.get("/register",function(req,res){
+    res.render("register")
+})
+app.post("/register",function(req,res){
+    const newUser =new user({
+        email : req.body.username,
+        password : req.body.password
+
+    })
+    newUser.save(function(err)
+    {
+        if(err)
+        {
+            console.log(err);
+        }
+        else{
+            res.render("secrets");
+        }
+    })
+})
+app.post("/login",function(req,res){
+ 
+       const  userName=req.body.username;
+      const password = req.body.password;
+      user.findOne({email:userName},function(err,foundUser)
+      {
+          if(err)
+          {
+              console.log(err);
+          }
+          else
+          {
+              if(foundUser)
+              {
+                  if(foundUser.password===password)
+                  {
+                    res.render("secrets");  
+                  }
+              }
+          }
+      })
+       
+})
+
+
+
+
+app.listen(3000,function(){
+
+    console.log("app started listening on port 3000");
+})
